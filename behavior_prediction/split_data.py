2e2ae52c33ae8aa5bac2a8a439b7101981486053
sy-chung <sy.alan.chung@gmail.com>
2020-10-12 20:52:28 +0900

    typo updates

--- behavior_prediction/split_data.py
+++ behavior_prediction/split_data.py
@@ -1,4 +1,3 @@
-import csv
 import os
 
 import numpy as np
@@ -13,15 +12,16 @@ RAW_LABELS = {
   'Stroke': 4
 }
 PRO_LABELS = {
-    'H' : 0,
-    'D' : 1,
-    'P' : 2,
-    'R' : 3,
-    'S' : 4
+    'H': 0,
+    'D': 1,
+    'P': 2,
+    'R': 3,
+    'S': 4
 }
 
+
 WINDOW_SIZE = 8
-SPLIT_SIZE = 0.3 # 30%
+SPLIT_SIZE = 0.3  # 30%
 
 
 def split_data(data_name_list, given_labels, data_type, window_size, split_size, test_label_path):
@@ -32,11 +32,10 @@ def split_data(data_name_list, given_labels, data_type, window_size, split_size,
     for data_name in data_name_list:
         data = []
 
-        raw_data_path = os.path.join('dataset/original', '%s_%s.csv' %(data_name, data_type))
+        raw_data_path = os.path.join('dataset/original', '%s_%s.csv' % (data_name, data_type))
         with open(raw_data_path, 'r') as f:
             f.readline()
             for row in f:
-                # use rstrip() instead replace('\n', '')
                 data.append(row.rstrip().split(','))
 
         data_array = np.array(data)
@@ -51,7 +50,7 @@ def split_data(data_name_list, given_labels, data_type, window_size, split_size,
         # make sliding window array data
         window_data_array = []
         for i in range(0, len(data_array) - window_size, 1):
-            window_data_array.append(data_array[i : i + window_size, :])
+            window_data_array.append(data_array[i: i + window_size, :])
         window_data_array = np.array(window_data_array)
         # print(np.shape(window_data_array))
         # : (x, 8, 20) for raw, (x, 8, 33) for processed
@@ -69,19 +68,19 @@ def split_data(data_name_list, given_labels, data_type, window_size, split_size,
 
         if data_type == 'raw':
             train_data = window_data_array[:num_train_data, :, :-2]
-            print(np.shape(train_data)) #: (x, 8, 18)
+            print(np.shape(train_data))  #: (x, 8, 18)
             train_label = window_data_array[:num_train_data, :, -1]
             train_label = train_label.reshape((-1, WINDOW_SIZE, 1))
-            print(np.shape(train_label)) #: (x, 8, 1)
+            print(np.shape(train_label))  #: (x, 8, 1)
 
-            test_data = window_data_array[num_train_data: , :, :-2]
-            print(np.shape(test_data)) # : (x, 8, 18)
+            test_data = window_data_array[num_train_data:, :, :-2]
+            print(np.shape(test_data))  # : (x, 8, 18)
             test_label = window_data_array[num_train_data:, :, -1]
             test_label = test_label.reshape((-1, WINDOW_SIZE, 1))
-            print(np.shape(test_label)) # : (x, 8, 1)
+            print(np.shape(test_label))  # : (x, 8, 1)
         else:
             train_vel_data = window_data_array[:num_train_data, :, :12]
-            print(np.shape(train_vel_data)) #: (x, 8, 12)
+            print(np.shape(train_vel_data))  #: (x, 8, 12)
             train_vel_label = window_data_array[:num_train_data, :, -1]
             train_vel_label = train_vel_label.reshape((-1, WINDOW_SIZE, 1))
             print(np.shape(train_vel_label))
@@ -118,21 +117,21 @@ def split_data(data_name_list, given_labels, data_type, window_size, split_size,
             num_test_data = 0
             for line in test_label:
                 for value in line:
-                    test_label_file.write('%d, %d\n' %(num_total_test_data + 1, given_labels[value[-1]]))
+                    test_label_file.write('%d, %d\n' % (num_total_test_data + 1, given_labels[value[-1]]))
                     num_test_data += 1
                     num_total_test_data += 1
-            print('%s %s: %d test data' %(data_name, data_type, num_test_data))
+            print('%s %s: %d test data' % (data_name, data_type, num_test_data))
         else:
             num_test_data = 0
             for line in test_vel_label:
                 for value in line:
-                    test_label_file.write('%d, %d\n' %(num_total_test_data + 1, given_labels[value[-1]]))
+                    test_label_file.write('%d, %d\n' % (num_total_test_data + 1, given_labels[value[-1]]))
                     num_test_data += 1
                     num_total_test_data += 1
-            print('%s %s: %d test data' %(data_name, data_type, num_test_data))
+            print('%s %s: %d test data' % (data_name, data_type, num_test_data))
 
     test_label_file.close()
-    print('total number of test data = %d' %(num_total_test_data))
+    print('total number of test data = %d' % num_total_test_data)
 
 
 def write_data(data, label, data_name, data_type, index, sub_type):
@@ -141,9 +140,9 @@ def write_data(data, label, data_name, data_type, index, sub_type):
         os.makedirs(train_test_path)       
     # write data
     if sub_type is None:
-        data_path = os.path.join(train_test_path, '%s_%s_%s.csv' %(data_name, data_type, index))
+        data_path = os.path.join(train_test_path, '%s_%s_%s.csv' % (data_name, data_type, index))
     else:
-        data_path = os.path.join(train_test_path, '%s_%s_%s_%s.csv' %(data_name, data_type, sub_type, index))
+        data_path = os.path.join(train_test_path, '%s_%s_%s_%s.csv' % (data_name, data_type, sub_type, index))
     with open(data_path, 'w') as f:
         for line in data:
             for value in line:
@@ -152,9 +151,9 @@ def write_data(data, label, data_name, data_type, index, sub_type):
 
     # write label
     if sub_type is None:
-        label_path = os.path.join(train_test_path, '%s_%s_%s_label.txt' %(data_name, data_type, index))
+        label_path = os.path.join(train_test_path, '%s_%s_%s_label.txt' % (data_name, data_type, index))
     else:
-        label_path = os.path.join(train_test_path, '%s_%s_%s_%s_label.txt' %(data_name, data_type, sub_type, index))
+        label_path = os.path.join(train_test_path, '%s_%s_%s_%s_label.txt' % (data_name, data_type, sub_type, index))
     with open(label_path, 'w') as f:
         for line in label:
             for value in line:
